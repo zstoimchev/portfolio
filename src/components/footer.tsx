788c65f62eba4b92bccd43734a19997146286cfd
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-09-19 23:59:27 +0200

    changed footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,26 @@ export default function Footer() {
 
     return (
         <>
-            <footer className="bg-gray-800 p-4 mt-4 content">
-                <div className="container mx-auto text-center text-white">
-                    <p>© 2024 Zhivko Stoimchev. All rights reserved.</p>
-                    <div className="flex justify-center space-x-4 mt-2">
-                        <Link href="/#" className="text-amber-200">Privacy Policy</Link>
-                        <Link href="#" className="text-amber-200">Terms of Service</Link>
-                    </div>
+            <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 content">
+                <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024
+                        <a href="https://flowbite.com/" className="hover:underline"> Zhivko Stoimchev™</a>
+                        . All Rights Reserved.
+                    </span>
+                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline">Contact</a>
+                        </li>
+                    </ul>
                 </div>
             </footer>
         </>
