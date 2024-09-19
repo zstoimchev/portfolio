cee95943d32ad5c89e5b03858bc9710fb3af28c4
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-09-20 01:14:32 +0200

    fixed the classes, no more main div, maybe is needed who knows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,16 @@
 export default function Home() {
     return (
-        <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-vt323-regular)]"
-        >
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-
+        <div className="flex flex-col mt-10 gap-8 items-center sm:items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
+            <div className="gap-0 items-center flex flex-col">
+                <p>_hello friend, my favourite Data Structure is Bloom Filter. I&apos;m</p>
                 <h1 className="text-7xl">&#123;&quot; Zhivko Stoimchev &quot;&#125;</h1>
-                <h1 className="text-7xl">&#123;&quot; Zhivko Stoimchev &quot;&#125;</h1>
-                <h1 className="text-7xl">&#123;&quot; Zhivko Stoimchev &quot;&#125;</h1>
-
-            </main>
+            </div>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
+            <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
         </div>
     );
 }
-
-// export default function Home() {
-//     return (
-//         <div className="flex flex-col gap-8 items-center sm:items-start p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
-//             <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
-//             <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
-//             <h1 className="text-7xl">{" Zhivko Stoimchev "}</h1>
-//         </div>
-//     );
-// }
