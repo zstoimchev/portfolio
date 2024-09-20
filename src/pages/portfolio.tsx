8f6ca4c7d0856449ef2a4b2cb352417682beda98
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-09-20 12:00:32 +0200

    added portfolio page for projects and corrected the div class

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,9 +1,9 @@
-export default function About() {
+export default function Portfolio() {
     return (
         <div
             className="flex flex-col mt-10 gap-10 items-center sm:items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
 
-            <h1 className="text-2xl">This is ABOUT page!</h1>
+            <h1 className="text-2xl">This is PORTFOLIO page!</h1>
 
         </div>)
 }
