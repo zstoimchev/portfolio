b66070d2367e9249e7238eb7283798ba356fb6e8
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-09-20 02:32:45 +0200

    fixed typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,13 +41,13 @@ export default function Home() {
         <div
             className="flex flex-col mt-10 gap-10 items-center sm:items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
             <div className="gap-0 items-center flex flex-col">
-                <p>Hello friend, glad you are here! My name is</p>
+                <p>&#47;&#47; Hello friend, glad you are here! My name is</p>
                 {/*<h1 className="text-8xl">&#123;&quot; Zhivko Stoimchev &quot;&#125;</h1>*/}
                 <h1 className="text-8xl">&lt; Zhivko Stoimchev &gt;</h1>
             </div>
 
             <div className="gap-0 items-center flex flex-col">
-                <p>I am working as</p>
+                <p>&#47;&#47; I am working as</p>
                 <h1 className="text-6xl">&#123;&quot; _{displayText} &quot;&#125;</h1>
             </div>
 
