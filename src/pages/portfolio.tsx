8e60ada5ed3cdc876c913f71917986654484571e
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-10-06 00:13:24 +0200

    added title

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -2,6 +2,7 @@ export default function Portfolio() {
     return (
         <div
             className="flex flex-col mt-10 gap-10 items-center sm:items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
+            <h1 className="text-5xl">&lt; About me &gt;</h1>
 
             <h1 className="text-2xl">This is PORTFOLIO page!</h1>
 
