76bc53a275634fbfce1f303b93a145e1faf717e7
Zhivko Soimchev <zivkostoimcev1551@outlook.com>
2024-10-06 00:12:53 +0200

    added title

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,18 +2,21 @@ export default function About() {
     return (
         <div
             className="flex flex-col mt-10 gap-10 items-center sm:items-center p-8 pb-20 sm:p-20 font-[family-name:var(--font-vt323-regular)]">
+            <h1 className="text-5xl">&lt; About me &gt;</h1>
 
             <div className="widdd">
                 <div className="pb-5">
                     <h1 className="text-2xl">&gt;_ short introduction</h1>
-                    <p className="text-justify">Hello Human, it's me, Zhivko! My favorite data structure is Bloom Filter, and you? Perfect. Nice
+                    <p className="text-justify">Hello Human, it's me, Zhivko! My favorite data structure is Bloom
+                        Filter, and you? Perfect. Nice
                         to meet you.
                     </p>
                 </div>
 
                 <div className="pb-5 ">
                     <h1 className="text-2xl">&gt;_ passion and dedication</h1>
-                    <p className="text-justify">Since my early days in school, I have developed a passion for system security and integrity.
+                    <p className="text-justify">Since my early days in school, I have developed a passion for system
+                        security and integrity.
                         This led me to explore different operating systems and their vulnerabilities, such as Windows,
                         alongside Linux and its many distributions. I like taking new challenges and learning from my
                         mistakes. Currently I am focused on networking, building mini HomeLab, with passion to continue
@@ -23,7 +26,8 @@ export default function About() {
 
                 <div className="pb-5">
                     <h1 className="text-2xl">&gt;_ methods and efficiency</h1>
-                    <p className="text-justify">I strive for simplicity and clarity in my programming endeavors, adhering to the KISS principle,
+                    <p className="text-justify">I strive for simplicity and clarity in my programming endeavors,
+                        adhering to the KISS principle,
                         therefore saving considerable time in debugging my applications. This principle guides me to
                         create solutions that are both efficient and easy to understand. All the projects are visible on
                         my GitHub profile.
@@ -32,7 +36,8 @@ export default function About() {
 
                 <div>
                     <h1 className="text-2xl">&gt;_ contact and cooperation</h1>
-                    <p className="text-justify">Have any questions? Not a problem at all, just send me an email, I tend to respond as soon as I
+                    <p className="text-justify">Have any questions? Not a problem at all, just send me an email, I tend
+                        to respond as soon as I
                         read it. Enjoy the work I've done so far. Cheers!
                     </p>
                 </div>
